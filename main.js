1d3a9a28ab9b386bac1a5918c575c62020b7d5bc
Puttaporn-Luangseephet <puttaporn.l@ku.th>
2025-01-27 13:48:44 +0700

    html form edit

---
+++ main.js
@@ -0,0 +1,13 @@
+let student = [
+    {
+        name: 'John',
+        age: 25 ,
+        grade: 'A'
+    },
+    {
+        name: 'Jane',
+        age: 75,
+        grade: 'B'
+    }
+]
+console.log(student[0]) ;
